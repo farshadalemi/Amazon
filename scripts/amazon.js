7d56cb1e3d8f788671007ce6e6163fcfc8b15c2d
Farshad <farshad.alemi@gmail.com>
2023-06-26 13:05:47 +0330

    cart functionality

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -106,18 +106,37 @@ products.forEach((product) => {
     <button class="
     add-to-cart-button 
     button-primary 
-    js-add-to-cart" date-product-name="${product.name}">
+    js-add-to-cart"
+    data-product-id="${product.id}">
       Add to Cart
     </button>
   </div>
     `;
 });
 
-console.log(productsHTML);
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(button);
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+
+    console.log(cart);
   });
 });
